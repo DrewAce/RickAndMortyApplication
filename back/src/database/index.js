2ff756bf25684e81b35b74da608c3bed4fe4919f
unknown <juancarmona3090@gmail.com>
2023-03-13 19:28:10 -0500

    Fixing Rick and morty Database

--- back/src/database/index.js
+++ back/src/database/index.js
@@ -1,13 +1,14 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const { characterModel } = require("./models/Character");
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
-
-const sequelize = new Sequelize(
-    `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/rickandmorty`,
-    { logging: false, native: false }
-  );
-
+const {
+  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT,
+} = process.env;
+console.log(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`);
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`, {
+  logging: false, // set to console.log to see the raw SQL queries
+  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+});
 characterModel(sequelize)
 
 
